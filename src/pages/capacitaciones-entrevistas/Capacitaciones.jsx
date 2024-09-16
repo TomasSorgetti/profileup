609bb8cbb9f59b667a6f7f15dec670aff3cc1bd3
Marcos <lavarellomarcos@gmail.com>
2024-09-16 12:39:20 -0300

    first commit

---
+++ src/pages/capacitaciones-entrevistas/Capacitaciones.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Capacitaciones = () => {
+  return (
+    <div>
+        <h1>Capacitaciones</h1>
+    </div>
+  )
+}
+
+export default Capacitaciones
---
+++ src/pages/terapia-piscologica/Terapia.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Terapia = () => {
+  return (
+    <div>
+      <h1>Terapia</h1>
+    </div>
+  )
+}
+
+export default Terapia
