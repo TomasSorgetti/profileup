bf80f2a6511b24713efcf5aadbf34f15330fcc2d
TomasSorgetti <tomassorgetti456@gmail.com>
2024-09-27 16:55:12 -0300

    refactor(components): Se separó en carpetas los componentes y assets y se hizo el form del banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,54 @@
-import './App.css'
+import "./App.css";
 
 // Define pages routes
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
 // Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // pages
-import Home from './pages/home/Home'
-import About from './pages/about/About'
-import Contacto from './pages/contacto/Contacto'
-import Rrhh from './pages/rrhh/Rrhh'
-import Terapia from './pages/terapia-piscologica/Terapia'
-import Asesoria from './pages/asesoria-consultoria/Asesoria'
-import Psicotecnico from './pages/psicotecnico-aptopsicologico/Psicotecnico'
-import Capacitaciones from './pages/capacitaciones-entrevistas/Capacitaciones'
-import Error from './pages/error-page/Error'
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contacto from "./pages/contacto/Contacto";
+import Rrhh from "./pages/rrhh/Rrhh";
+import Terapia from "./pages/terapia-piscologica/Terapia";
+import Asesoria from "./pages/asesoria-consultoria/Asesoria";
+import Psicotecnico from "./pages/psicotecnico-aptopsicologico/Psicotecnico";
+import Capacitaciones from "./pages/capacitaciones-entrevistas/Capacitaciones";
+import Error from "./pages/error-page/Error";
 
 // navbar
-import NavigationBar from './components/navbar/Navbar'
+import NavigationBar from "./components/layout/navbar/Navbar";
 
 // Footer
-import Footer from './components/footer/Footer'
+import Footer from "./components/layout/footer/Footer";
 
 function App() {
-
   return (
     <>
+      <NavigationBar />
 
-    <NavigationBar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/rrhh" element={<Rrhh />} />
+        <Route path="/terapia-piscologica" element={<Terapia />} />
+        <Route path="/asesoria-consultoria" element={<Asesoria />} />
+        <Route
+          path="/psicotecnico-aptopsicologico"
+          element={<Psicotecnico />}
+        />
+        <Route
+          path="/capacitaciones-entrevistas"
+          element={<Capacitaciones />}
+        />
+        <Route path="*" element={<Error />} />
+      </Routes>
 
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/contacto' element={<Contacto/>} />
-      <Route path='/rrhh' element={<Rrhh/>} />
-      <Route path='/terapia-piscologica' element={<Terapia/>} />
-      <Route path='/asesoria-consultoria' element={<Asesoria/>} />
-      <Route path='/psicotecnico-aptopsicologico' element={<Psicotecnico/>} />
-      <Route path='/capacitaciones-entrevistas' element={<Capacitaciones/>} />
-      <Route path='*' element={<Error/>} />
-    </Routes>
-
-    <Footer/>
+      <Footer />
     </>
-
-    
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/layout/footer/Footer.jsx
@@ -0,0 +1,81 @@
+// Crucial
+import React from "react";
+
+// Navigation library
+import { Link, NavLink } from "react-router-dom";
+
+// Logo image import
+import logoLight from "../../../assets/images/profielUp_logo-light.png";
+
+// Footer CSS
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={`page-footer font-small blue pt-4 ${styles.footer}`}>
+      <div
+        className={`container-fluid text-center text-md-left ${styles.footer_main_content}`}
+      >
+        <div className="row">
+          <div className="col-md-3 mt-md-0 mt-3">
+            <div className={styles.logo_footer_container}>
+              <img
+                src={logoLight}
+                alt="logo ProfileUp"
+                className={styles.logo_footer}
+              />
+            </div>
+          </div>
+
+          <hr className="clearfix w-100 d-md-none pb-0" />
+
+          <div className="col-md-3 mb-md-0 mb-3">
+            <h5>Información</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#!">contacto@mail.com</a>
+              </li>
+              <li>
+                <a href="#!">(+54) 11 3454-4545</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-3 mb-md-0 mb-3">
+            <h5>Contacto</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#!">contacto@mail.com</a>
+              </li>
+              <li>
+                <a href="#!">(+54) 11 3454-4545</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-md-3 mb-md-0 mb-3">
+            <h5>Redes sociales</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#!">Linkedin</a>
+              </li>
+              <li>
+                <a href="#!">Instagram</a>
+              </li>
+              <li>
+                <a href="#!">whatsapp</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="footer-copyright text-center py-3">
+        Todos los derechos reservados a Matías Martínez © Copyright 2024
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
