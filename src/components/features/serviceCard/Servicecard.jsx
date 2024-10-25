17b2c2947c94b7ac72519aa0f072c5d5979cfd7e
TomasSorgetti <tomassorgetti456@gmail.com>
2024-10-25 10:11:54 -0300

    refactor: Se cambió el estilo del button de la card

--- src/components/features/serviceCard/Servicecard.jsx
+++ src/components/features/serviceCard/Servicecard.jsx
@@ -1,29 +1,29 @@
 // Crucial
-import React from 'react'
+import React from "react";
 
 // Bootstrap elements
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 // Navigation library
-import { Link,NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom";
 
 // Servicecard CSS
-import styles from './Servicecard.module.css'
+import styles from "./Servicecard.module.css";
 
-const Servicecard = ({title, description, src, link}) => {
+const Servicecard = ({ title, description, src, link }) => {
   return (
-    <Card style={{ width: '18rem' }} className={styles.card}>
-        <img src={src} alt="" className='card-img-top'/>
-      <Card.Body>
+    <Card style={{ width: "18rem" }} className={styles.card}>
+      <img src={src} alt="" className="card-img-top" />
+      <Card.Body className="d-flex flex-column justify-content-between align-items-start">
         <Card.Title>{title}</Card.Title>
-        <Card.Text>
-          {description}
-        </Card.Text>
-        <Link className='btn btn-primary' to={link}>Más información</Link>
+        <Card.Text>{description}</Card.Text>
+        <Link to={link}>
+          Más información
+        </Link>
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default Servicecard
+export default Servicecard;
